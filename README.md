2a16e7a2a5de1f62409b9d38368c1e5753e9f020
UltCombo <ult_combo@hotmail.com>
2013-10-25 20:24:43 -0200

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,37 @@
 poly-checked
 ============
 
-Miraculous :checked pseudo-class polyfill
+Miraculous `:checked` pseudo-class polyfill.
+
+## How to use
+
+Include jQuery 1.x (compatible with old IE) >= 1.7, then the poly-checked script inside an IE conditional comment:
+
+```html
+<script src="://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<!--[if lte IE 8]>
+<script src="path/to/poly-checked.min.js"></script>
+<![endif]-->
+```
+
+Make sure to include all stylesheets before the script and that's all!
+
+
+## Features
+
+- Transparent to the developer, no need to add ugly polyfill classes to your CSS.
+- Works with dynamically added checkbox/radio elements.
+- Full interoperability with JavaScript. Like the real `:checked` pseudo-class, changing an element's `checked` state will automatically update its style.
+- Keeps the same selector specifity as your original selectors.
+- Bonus: extra patch for `:checked` to work with sibling selectors. Very useful for customized CSS checkboxes/radios.
+
+## Compatibility
+
+Currently tested in IE 7 and 8.
+
+## Known limitations & workarounds
+
+- Can't polyfill stylesheets from different domains. There's no DOM API to retrieve unrecognized selectors nor raw CSS text from a `<link rel="stylesheet">` element, hence the polyfill uses XHR which is subject to the same-origin policy. The workaround is to host all stylesheets that require polyfilling in the same domain. Feel free to submit a PR or open an issue for discussion if you find a better alternative.
+- Detecting dynamically added elements is done through long polling, which may cause some FOUC. jQuery's `.clone(true)` can be used on an already patched checkbox/radio to avoid the milliseconds of FOUC. It'd best if we could get rid of the polling altogether, but I haven't found any evented way to detect newly created input elements. Feel free to open a PR/issue as usual if there's any better way to handle this in old IE.
+- Does not polyfill dynamically added stylesheets. This is a rare use case and I don't fill like adding more DOM polling here, but it is very possible to implement. Feel free to open a PR/issue if you have a non-polling solution.
+- Does not polyfill the `:checked` pseudo-class for `option` elements (`option:checked`). Option elements are rather limited and cross-browser inconsistent for styling, hence this is not one of my priorities. You can always use one of the thousands of styled select/combobox plugins available out there.
---
+++ poly-checked.js
@@ -0,0 +1,75 @@
+// NOTE: This script is meant for IE<=8 ONLY. Make sure to include it using conditional comments if you don't want errors in modern browsers.
+// Requires jQuery 1.x (compatible with IE7-8) version >= 1.7 (tested up to 1.10.2)
+// Avoid loading this script asynchronously, unrecognized selectors in inline style elements are lost after DOM ready
+(function($) {
+
+	var CHECKED_CLASS = 'poly-checked',
+		JUST_CHANGED_PARENT_CLASS = 'poly-just-changed',
+		PATCHED_DATA_PROP = 'isPolyChecked',
+		inputsLiveNodeList = document.getElementsByTagName('input');
+
+	function patchStylesheet(elem, text) {
+		// A global replace here is a bit hackish, but works for the great majority of use cases.
+		// Issue: may have unintended effects on CSS strings (URLs, content property value) containing the ":checked" substring.
+		// Workaround: Use an escape sequence: "\00003Achecked" and "\3A checked" will both be print ":checked"
+		//(U+0020 space characters immediately following a hexadecimal escape sequence are automatically consumed by the escape sequence)
+		elem.styleSheet.cssText = text.replace(/:checked/g, '.' + CHECKED_CLASS);
+	}
+
+	$('style').each(function() {
+		// Retrieving the style element's innerHTML only works before DOM ready,
+		// after DOM ready the rules with invalid selectors are simply removed from the style's innerHTML. (dafaq microsoft??)
+		patchStylesheet(this, this.innerHTML);
+	});
+
+	$('link[rel=stylesheet]').each(function() {
+		var elem = this;
+		// There's apparently no other way to retrieve an external stylesheet's text content other than XHR.
+		// Issue: can't polyfill stylesheets from other domains due to same-origin policy.
+		// Workaround: host all stylesheets that need polyfilling in the same domain from where they're being <link>ed to.
+		$.get(this.href, function(rawCSSText) {
+			patchStylesheet(elem, rawCSSText);
+		});
+	});
+
+	function patchInput(elem) {
+		// Every input has unchecked styling until patched, so we only need to refresh the checked ones' styling.
+		if (elem.checked) refreshStyling(elem);
+
+		// propertychange fires due to both user action and JS code, which is ideal for our use case.
+		// Too bad it doesn't bubble though, hence we need to patch each input manually. See patchNonPatchedInputs below.
+		$(elem).on('propertychange._polychecked', function(e) {
+			if (e.originalEvent.propertyName === 'checked') {
+				refreshStyling(this);
+			}
+		}).data(PATCHED_DATA_PROP, 1);
+	}
+
+	function refreshStyling(elem) {
+		$(elem).toggleClass(CHECKED_CLASS, elem.checked)
+			//and now we force IE to execute selectors matching again so that sibling selectors work
+			.parent().addClass(JUST_CHANGED_PARENT_CLASS).removeClass(JUST_CHANGED_PARENT_CLASS);
+	}
+
+	function patchNonPatchedInputs() {
+		// As this will be used for long polling, using as much native JS as possible should provide better performance.
+		for (var i = 0; i < inputsLiveNodeList.length; i++) {
+			var input = inputsLiveNodeList[i];
+			if ((input.type === 'checkbox' || input.type === 'radio') && !$.data(input, PATCHED_DATA_PROP)) {
+				patchInput(input);
+			}
+		}
+	}
+
+	$(function() {
+		// After refresh, MSIE7-8 restores the checked state of checkboxes on DOM ready without triggering change events,
+		// hence we do it here. This also takes care of inputs with the [checked] attribute.
+		patchNonPatchedInputs();
+
+		// Okay, this is far from ideal, I'd very much prefer an evented or delegated way to do this but it is IMPOSSIBRU.
+		// - IE<9 do not support DOM Mutation Observers (which would also have terrible performance anyway);
+		// - NodeLists don't have any event or method to handle newly added elements (why is it even a live NodeList?);
+		setInterval(patchNonPatchedInputs, 200);
+	});
+
+}(jQuery));
